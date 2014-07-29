c308d9f73df5d8e133be59520ca6c0c52262206a
reddo <reddo@Flying-Jan-Puu>
2014-07-29 07:39:13 -0300

    Fiz Versioning

--- app/Application.js
+++ app/Application.js
@@ -28,7 +28,7 @@ function Application (debug) {
      * Minor covers new functions.
      * Release covers bugfixes only.
      */
-    this.version = [0, 13, 0];
+    this.version = [0, 12, 7];
     
     /**
      * Databases
