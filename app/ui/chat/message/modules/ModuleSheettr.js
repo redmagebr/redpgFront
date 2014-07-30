fa133791c0b2f8f4ee857ccdcb1eb17d5d809014
reddo <reddo@Flying-Jan-Puu>
2014-07-30 05:57:25 -0300

    Fix: Sheet Scroll? Add: Sheet Turn module

---
+++ app/ui/chat/message/modules/ModuleSheettr.js
@@ -0,0 +1,70 @@
+/**
+ * 
+ * If not making a Module for personal use, consider using the Language module to print any messages the module has.
+ */
+window.chatModules.push({
+
+    ID : 'sheettr',
+    
+    Slash : [],
+    
+    
+    isValid : function (slashCMD, message) {
+        return false;
+    },
+
+    /**
+     * @param {Message} msg
+     * @returns {jQuery}
+     */
+    get$ : function (msg) {
+        var user = msg.getUser();
+        var $msg = $('<p class="chatDano" />');
+        
+        var $persona = $('<b />').text(msg.getSpecial('sheetname', '????'));
+        $msg.
+            append($('<span class="turnIcon" />')).
+            append($persona).
+            append(":");
+        
+        if (user === null) {
+            user = new User();
+            user.nickname = '?';
+            user.nicknamesufix = '?';
+        }
+        var $tooltip = $('<span class="tooltip" />');
+        if (user.isStoryteller()) {
+            $tooltip.append($('<b class="language" data-langhtml="_STORYTELLERTOOLTIP_" />'));
+        } else {
+            $tooltip.append($('<b class="language" data-langhtml="_PLAYERTOOLTIP_" />'));
+        }
+
+        $tooltip.append(': ' + user.nickname + '#' + user.nicknamesufix);
+
+        $msg.append($tooltip);
+        
+        if (msg.id !== null) {
+            $msg.attr('data-msgid', msg.id);
+        } else {
+            msg.bindSaved(window.app.emulateBind(
+                function () {
+                    this.$msg.attr('data-msgid', this.msg.id);
+                }, {$msg : $msg, msg : msg}
+            ));
+        }
+        
+        var player = msg.getSpecial('player', 0);
+        if (window.app.loginapp.user.id === player && !window.app.ui.chat.cc.firstPrint) {
+            var audio = document.getElementById('yourTurnAudio');
+            audio.currentTime = 0;
+            audio.volume = 1;
+            audio.play();
+        }
+        
+        return $msg;
+    },
+    
+    getMsg : function (slashCMD, message) {
+        return null;
+    }
+});
