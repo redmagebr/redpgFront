fc9aa3415567232c4d8285387d5b2541419f2d0d
reddo <reddo@Flying-Jan-Puu>
2014-08-22 19:41:37 -0300

    Versioning.

--- app/Application.js
+++ app/Application.js
@@ -31,7 +31,7 @@ function Application (debug) {
      * Minor covers new functions.
      * Release covers bugfixes only.
      */
-    this.version = [0, 19, 2];
+    this.version = [0, 19, 3];
     
     /**
      * Databases
