d1b3283d02e775b05bdd407e75fb55e77b9ab4cb
reddo <reddo@Flying-Jan-Puu>
2014-09-06 22:55:55 -0300

    Remove Clear Sans files. Switch from mangled SquirrelFont's WOFF to the originals TTF and OTF for Alegreya and Caudex. Since Caudex has support to most languages, might be worth keeping it here forever.

--- app/Application.js
+++ app/Application.js
@@ -30,7 +30,7 @@ function Application (debug) {
      * Minor covers new functions.
      * Release covers bugfixes only.
      */
-    this.version = [0, 25, 3];
+    this.version = [0, 25, 4];
     
     /**
      * Databases
