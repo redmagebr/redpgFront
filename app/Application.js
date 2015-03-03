9cc655b7c00af0666789bff5c833646a96d4ec6a
Reddo <Reddo@192.168.25.13>
2015-03-03 05:42:40 -0300

    0.42.1

--- app/Application.js
+++ app/Application.js
@@ -37,7 +37,7 @@ function Application (debug) {
      * Minor covers new functions.
      * Release covers bugfixes only.
      */
-    this.version = [0, 42, 0];
+    this.version = [0, 42, 1];
     
     /**
      * Settings
