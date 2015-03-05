044a97b70481cfa89dbf394642ddd1053bfc2234
redmagebr <redmagebr@hotmail.com>
2015-03-04 23:19:36 -0300

    0.42.2

--- app/Application.js
+++ app/Application.js
@@ -37,7 +37,7 @@ function Application (debug) {
      * Minor covers new functions.
      * Release covers bugfixes only.
      */
-    this.version = [0, 42, 1];
+    this.version = [0, 42, 2];
     
     /**
      * Settings
