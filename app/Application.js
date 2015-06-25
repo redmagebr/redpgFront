b78ec732c6811b89db85700d067e137e3d07078b
Reddo <Reddo@Jan-Puu>
2015-06-25 15:23:05 -0300

    Versioning

--- app/Application.js
+++ app/Application.js
@@ -11,7 +11,7 @@ function Application (debug) {
      * Minor covers new functions.
      * Release covers bugfixes only.
      */
-    this.version = [0, 56, 8];
+    this.version = [0, 56, 10];
     
     if (typeof debug === 'undefined' || debug) {
         this.debug = true;
